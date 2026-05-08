755aacb16dd257a769ea8220e84eba9d1922a7a2
XiomaraJimenezz <est.nicoll.jimenez@unimilitar.edu.co>
2026-05-08 12:38:20 -0500

    Implementar algoritmo DDA para líneas

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ const H = canvas.height;
 function transformY(y){
     return H - y;
 }
+function drawLine(x1,y1,x2,y2,color){
+    let dx = x2 - x1;
+    let dy = y2 - y1;
+    let steps = Math.max(
+        Math.abs(dx),
+        Math.abs(dy)
+    );
+    let xIncrement = dx / steps;
+    let yIncrement = dy / steps;
+    let x = x1;
+    let y = y1;
+    ctx.fillStyle = color;
+    for(let i=0; i<=steps; i++){
+        ctx.fillRect(
+            Math.round(x),
+            Math.round(transformY(y)),
+            2,
+            2
+        );
+        x += xIncrement;
+        y += yIncrement;
+    }
+}
